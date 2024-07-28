b947b047022ae07add1d2938e9b2b218731f502f
vellimir <117735531+veellimir@users.noreply.github.com>
2024-07-28 16:03:53 +0400

    Update project_config.py

--- app/config/project_config.py
+++ app/config/project_config.py
@@ -30,7 +30,7 @@ class DatabaseConfig(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env-dev",
+        env_file=".env",
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
